187a2ce10d22903860614fc09d3457c85655c57f
chenjing <jingjingbic@gmail.com>
2019-03-01 13:46:47 -0800

    Some refactoring.

---
+++ src/main/java/org/ndexbio/interactomesearch/InteractomeResultNetworkSummary.java
@@ -0,0 +1,46 @@
+package org.ndexbio.interactomesearch;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class InteractomeResultNetworkSummary {
+	private int edgeCount;
+	private int nodeCount;
+	private long parentNodeCount;
+	private long parentEdgeCount;
+	
+	public InteractomeResultNetworkSummary() {}
+
+	public int getEdgeCount() {
+		return edgeCount;
+	}
+
+	public void setEdgeCount(int edgeCount) {
+		this.edgeCount = edgeCount;
+	}
+
+	public int getNodeCount() {
+		return nodeCount;
+	}
+
+	public void setNodeCount(int nodeCount) {
+		this.nodeCount = nodeCount;
+	}
+
+	public long getParentNodeCount() {
+		return parentNodeCount;
+	}
+
+	public void setParentNodeCount(long parentNodeCount) {
+		this.parentNodeCount = parentNodeCount;
+	}
+
+	public long getParentEdgeCount() {
+		return parentEdgeCount;
+	}
+
+	public void setParentEdgeCount(long parentEdgeCount) {
+		this.parentEdgeCount = parentEdgeCount;
+	}
+}
---
+++ src/main/java/org/ndexbio/interactomesearch/InteractomeSearchResult.java
@@ -0,0 +1,49 @@
+package org.ndexbio.interactomesearch;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class InteractomeSearchResult {
+	private int rank;
+	private List<String> hitGenes;
+	private InteractomeResultNetworkSummary summary;
+	private String networkUUID;
+
+	public InteractomeSearchResult() {
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public List<String> getHitGenes() {
+		return hitGenes;
+	}
+
+	public void setHitGenes(List<String> hitGenes) {
+		this.hitGenes = hitGenes;
+	}
+
+	public InteractomeResultNetworkSummary getSummary() {
+		return summary;
+	}
+
+	public void setSummary(InteractomeResultNetworkSummary summary) {
+		this.summary = summary;
+	}
+
+	public String getNetworkUUID() {
+		return networkUUID;
+	}
+
+	public void setNetworkUUID(String networkUUID) {
+		this.networkUUID = networkUUID;
+	}
+}
